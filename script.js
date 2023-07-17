499bb59bae7816512b234bb8bb0702f365e837f6
maruf2023 <maruf1916810369@gmailcom>
2023-07-17 13:37:55 +0600

    my first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+  header.classList.toggle("sticky", window.scrollY > 0)
+});
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('bx-x')
+  navbar.classList.toggle('active')
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('bx-x')
+  navbar.classList.remove('active')
+}
